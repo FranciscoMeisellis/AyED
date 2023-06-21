4f2b75bf3486b09e8d053308aa41d65fba75d453
Francisco Meisellis <fmeisellis@frba.utn.edu.ar>
2023-06-21 12:28:22 -0300

    Add files via upload

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+#include <cmath>
 #include <iomanip>
 using namespace std;
 
-double calcularpi () {
-    double suma_rest=0.0;
-    double denominador=1.0;
-    int signo=1.0;
-    int cont=0;
-    while (cont<=1000000)
-    {
-        cont++;
-        suma_rest+=signo/denominador;
-        denominador+=2;
-        signo*=-1;
-    }
-    return suma_rest*4;
-}
-int main(){
-double valorpi=0;
-valorpi=calcularpi();
-cout << setprecision(7);
-cout << " pi con precision 6: " << valorpi << endl;
-return 0;
+int main() {
+    double pi = 0.0, antsum=0.0;
+    const double num = 0.0000001;  // Precisión requerida para los 6 primeros decimales
+    double term = 1.0;
+    int sign = 1;
+    int iteracion = 0;
+
+    do {
+        //pi += sign * term;
+        antsum=pi;
+        pi += (4.0 / term) * sign;
+        sign *= -1;
+        term += 2;
+        iteracion++;
+    } while (fabs(pi - antsum) > num);
+
+    //cout << "Valor de pi calculado: " << pi << endl;
+    cout << "Número de iteraciones: " << iteracion << endl;
+    cout << "Valor de pi calculado: " << setprecision(7) << trunc(pi*1000000)/1000000 << endl;
+    return 0;
 }
